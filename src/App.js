69e3ea42dcf16818c76bdb7428bca276b3cded00
Christian <kristian2015ok@gmail.com>
2024-06-24 15:12:59 +0700

    Add: Using Json server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    const fetchSolution = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/solutions");
+        const data = await response.json();
+        const randomSolution = data[Math.floor(Math.random() * data.length)];
+        setSolution(randomSolution);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchSolution();
+  }, [setSolution]);
+
   return (
     <div className="App">
       <h1>Wordle Clone React App</h1>
+      {solution && <p>Solution is {solution.word}</p>}
     </div>
   );
 }
