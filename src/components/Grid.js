01b7ed6fdb4ef5de77d069db381a9cd27bc86b21
Christian <kristian2015ok@gmail.com>
2024-06-25 14:48:24 +0700

    Add: Showing the current guess

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,6 +4,9 @@ const Grid = ({ guesses, currentGuess, turn }) => {
   return (
     <div>
       {guesses.map((g, i) => {
+        if (turn === i) {
+          return <Row key={i} currentGuess={currentGuess} />
+        }
         return <Row key={i} guess={g} />;
       })}
     </div>
